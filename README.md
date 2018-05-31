fb46824bec95109efe2799bc448143d23683f17f
Haohao Zhang <hyacinth.hao@foxmail.com>
2018-05-31 14:07:25 +0800

    init 0.1.0

---
+++ README.md
@@ -0,0 +1,17 @@
+# Remove Xnip Watermark Alfred Workflow
+
+![Demo](demo.gif?raw=true)
+
+## Installation
+
+required: Python3, Pillow, pastedboard
+
+1. Download this project.
+1. import workflow (Double click the workflow file).
+1. change variable `py3` and `cropscript` to yours (click the `[x]` button in the upper right corner of workflow window).
+
+![workflows tab](workflows_tab.png?raw=true)
+
+## Notes
+
+If you don't know how to use this workflow. you can remove the watermark in the screenshot by subscribing to Xnip Pro($1.99 per year).
---
+++ crop.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+# coding=utf-8
+# author: hyacz<hyacz@foxmail.com>
+# 2018-05-30 18:42
+# 
+
+import io
+import pasteboard
+from PIL import Image
+
+# define
+WM_H   = 40
+
+img_bytes = io.BytesIO()
+pb        = pasteboard.Pasteboard()
+
+# get image
+img       = Image.open(io.BytesIO(pb.get_contents(pasteboard.PNG)))
+
+# if retina display
+dpi = img.info.get('dpi', (96, 96))
+if dpi[0] >= 144:
+    WM_H *= 2
+
+# if too narrow
+if img.width <= 400:
+    exit()
+
+# crop
+img.crop((0, WM_H, img.width, img.height)).save(img_bytes, format='PNG', dpi=dpi)
+
+# set cilpboard
+pb.set_contents(img_bytes.getvalue(), type=pasteboard.PNG)
